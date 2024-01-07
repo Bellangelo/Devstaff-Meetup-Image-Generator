32a94b1521d31f6c1619dee5838a06deb9462828
Aggelos Bellos <aggelosbellos7@gmail.com>
2024-01-07 02:15:09 +0200

    Move index.js outside of main application code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {join, dirname} from 'path';
 import { fileURLToPath } from 'url';
-import App from './App.js';
+import App from './src/App.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
